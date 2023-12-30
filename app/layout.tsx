073fe36bb31505b97ab50a4bbd5eec8d5aae92a2
kitt-tha <kitt.connect.number@gmail.com>
2023-12-30 17:36:33 +0700

    Added my project

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+ 
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Coming soon powered by Krungthep Startup</title>
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+         <link href="style.css" rel="stylesheet" type="text/css" media="all" />
+          <link href="bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+export default function Home() {
+  return (
+      <div className="wrapper">
+        <div id="main" className="main">
+                <div className="home"> 
+                  <div className="container">
+                  <div className="row text-center">
+                  <div className="col-md-12">
+                    <div className="hero-img">
+                      <img className="img-fluid" src="wait.png" alt="Home" />
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                    <div className="hero-content">
+                      <h1>Weave from Krungthep Startup</h1>
+                      <h4> www.krungthep.com </h4>
+                      <p>💥🌈🎉🥰  Something Awesome is Coming Soon. 👈🍫🎖️</p>
+                       
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                <div class="client-list">
+                  <ul>
+                    <li><img className="img-fluid" src="1.png" /></li>
+                    <li><img className="img-fluid" src="2.png" /></li>
+                    <li><img className="img-fluid" src="3.png" /></li>
+                    <li><img className="img-fluid" src="4.png" /></li>
+                    <li><img className="img-fluid" src="5.png" /></li>
+                  </ul>
+                </div>
+              </div>
+
+                  </div>
+                  </div>
+                </div>
+        </div>
+
+
+             
+    </div>
+  )
+}
