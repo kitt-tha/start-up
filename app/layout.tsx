625abddad9457b39236ca5f7ea713c3972b966b7
kitt-tha <kitt.connect.number@gmail.com>
2023-12-30 20:13:21 +0700

    Fix 12302023813

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Coming soon powered by Krungthep Startup</title>
-        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+        <title>Coming soon powered by Krungthep Startup</title> 
          <link href="styles/style.css" rel="stylesheet" type="text/css" media="all" />
           <link href="styles/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
       </head>
